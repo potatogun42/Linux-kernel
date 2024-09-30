5a756cbc4b270c4494a07cfa2f53be196c01825b
b2202040 <86211838+potatogun42@users.noreply.github.com>
2024-09-30 14:42:43 +0900

    Update ftrace.sh

--- ftrace.sh
+++ ftrace.sh
@@ -3,7 +3,7 @@ cat /sys/kernel/debug/tracing/current_tracer
 echo 1 > /sys/kernel/debug/tracing/tracing_on
 echo '*sleep*' > /sys/kernel/debug/tracing/set_ftrace_filter
 echo function > /sys/kernel/debug/tracing/current_tracer
-sleep 10
+./t_sleep
 cat /sys/kernel/debug/tracing/trace > /tmp/out.trace01.txt
 echo nop > /sys/kernel/debug/tracing/current_tracer
 echo > /sys/kernel/debug/tracing/set_ftrace_filter
